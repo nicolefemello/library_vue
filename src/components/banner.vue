4a5a43d40dc3f429ad530e5b97594e4ba128eb9e
nicolefemello <nicfmello@gmail.com>
2025-04-29 13:40:07 -0300

    STYLE: add carousel in home's banner

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,22 +1,49 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
 import { defineProps } from 'vue'
 import { useCartStore } from '@/stores/cartStore'
 import type { IBook } from '@/types/booksTypes'
 
 const cartStore = useCartStore()
 const props = defineProps<{ products: IBook[] }>()
+
+const listRef = ref<HTMLElement | null>(null)
+let currentIndex = 0
+let intervalId: number
+
+onMounted(() => {
+  intervalId = window.setInterval(() => {
+    const list = listRef.value
+    if (!list) return
+
+    const children = list.children
+
+    currentIndex = (currentIndex + 1) % children.length
+    const scrollToElement = children[currentIndex] as HTMLElement
+
+    list.scrollTo({
+      left: scrollToElement.offsetLeft,
+      behavior: 'smooth',
+    })
+  }, 4000)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId)
+})
 </script>
 
+
 <template>
-  <ul class="flex overflow-x-auto lg:py-10">
+  <ul ref="listRef" class="flex overflow-x-auto scroll-smooth snap-x snap-mandatory">
     <li v-for="book in products" :key="book.id"
-      class="flex flex-col lg:flex-row justify-center items-center border-b border-[#27AE60] lg:gap-50 px-4 lg:px-10 min-w-full">
+      class="flex flex-col lg:flex-row justify-center items-center border-b border-[#27AE60] lg:gap-30 px-4 lg:px-10 w-full flex-shrink-0 snap-start">
       <div id="banner-left" class="grid gap-5 max-w-2xl text-center lg:text-left">
-        <p class="p-2 w-fit mx-auto lg:mx-0 text-lg text-[#27AE60] border border-[#27AE60]">
+        <p class="p-2 w-fit mx-auto lg:mx-0 text-sm text-[#27AE60] border border-[#27AE60]">
           Autor de Abril
         </p>
-        <h2 class="font-bold text-4xl md:text-6xl text-[#382C2C]">{{ book.volumeInfo.title }}</h2>
-        <p class="text-xl text-[#4D4C4C] line-clamp-5">{{ book.volumeInfo.description }}</p>
+        <h2 class="font-bold text-3xl md:text-5xl text-[#382C2C]">{{ book.volumeInfo.title }}</h2>
+        <p class="text-base text-[#4D4C4C] line-clamp-5">{{ book.volumeInfo.description }}</p>
         <button @click="
           () => {
             cartStore.addToCart(book)
@@ -33,7 +60,7 @@ const props = defineProps<{ products: IBook[] }>()
       </div>
 
       <img :src="book.volumeInfo.imageLinks?.thumbnail" :alt="book.volumeInfo.title"
-        class="w-full max-w-[300px] sm:max-w-[400px] sm:min-w-[250px] object-cover my-10" />
+        class="w-full max-w-[300px] sm:min-w-[250px] object-cover my-10" />
     </li>
   </ul>
 </template>
