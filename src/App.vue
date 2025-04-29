a7ed5a765a4bf98b1915d11e118e1635d09a5a85
matheusmichalski <matheus.michalskifc@gmail.com>
2025-04-29 14:06:31 -0300

    FEAT: add loading overlay and integrate loading state management

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import HeaderComp from './components/general/HeaderComp.vue';
+import HeaderComp from './components/general/HeaderComp.vue'
 import FooterComp from './components/general/FooterComp.vue'
+
+import { useLoadingStore } from '@/stores/loadingStore'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
+
+const loadingStore = useLoadingStore()
 </script>
 
 <template>
@@ -10,6 +16,7 @@ import FooterComp from './components/general/FooterComp.vue'
 
   <main>
     <RouterView />
+    <Loading :active="loadingStore.isLoading" :is-full-page="true" />
   </main>
 
   <footer class="py-5 bg-[#27AE60] text-white">
