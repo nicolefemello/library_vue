08e9e9819fdda157604a7a7eca19afd888864efe
matheusmichalski <matheus.michalskifc@gmail.com>
2025-05-09 08:14:05 -0300

    feat: add pinia-plugin-persistedstate for favorites management

--- src/components/menu-books.vue
+++ src/components/menu-books.vue
@@ -41,17 +41,19 @@ function addFavorite(book: IBook) {
 
 function isFavorite(book: IBook): boolean {
   const favorites = favoriteStore.favoriteProducts
-  return favorites.some(fav => fav.id === book.id)
+  return favorites.some((fav) => fav.id === book.id)
 }
-
 </script>
 
-
 <template>
-  <li class="grid w-[280px] h-[500px] rounded-lg border border-zinc-200 shadow-md hover:shadow-lg transition">
-    <img :src="book.volumeInfo.imageLinks?.thumbnail" :alt="book.volumeInfo.title"
-      class="w-full h-[280px] sm:h-[300px] object-cover rounded-t-lg" />
-
+  <li
+    class="grid w-[280px] h-[500px] rounded-lg border border-zinc-200 shadow-md hover:shadow-lg transition"
+  >
+    <img
+      :src="book.volumeInfo.imageLinks?.thumbnail"
+      :alt="book.volumeInfo.title"
+      class="w-full h-[280px] sm:h-[300px] object-cover rounded-t-lg"
+    />
 
     <div id="info-book" class="grid gap-3 p-3 sm:p-4">
       <h3 class="font-semibold text-base sm:text-lg text-[#382C2C] line-clamp-1">
@@ -59,33 +61,41 @@ function isFavorite(book: IBook): boolean {
       </h3>
       <p class="text-sm text-[#4F4C57] line-clamp-2">{{ book.volumeInfo.title }}</p>
 
-
       <div class="flex items-center justify-between">
-        <p :class="[
-          'flex justify-between items-center text-base',
-          book.saleInfo.listPrice?.amount === 0
-            ? 'text-green-500'
-            : book.saleInfo.listPrice?.amount == null
-              ? 'text-red-500'
-              : 'text-[#382C2C]',
-        ]">
+        <p
+          :class="[
+            'flex justify-between items-center text-base',
+            book.saleInfo.listPrice?.amount === 0
+              ? 'text-green-500'
+              : book.saleInfo.listPrice?.amount == null
+                ? 'text-red-500'
+                : 'text-[#382C2C]',
+          ]"
+        >
           {{ formatCurrency(book.saleInfo.listPrice?.amount) }}
         </p>
 
         <button @click="addFavorite(book)">
-          <span class="material-symbols-outlined cursor-pointer transition-all duration-200" :style="{
-            color: isFavorite(book) ? '#27AE60' : '#B0B0B0',
-            fontVariationSettings: `'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 24`
-          }">favorite</span>
+          <span
+            class="material-symbols-outlined cursor-pointer transition-all duration-200"
+            :style="{
+              color: isFavorite(book) ? '#27AE60' : '#B0B0B0',
+              fontVariationSettings: `'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 24`,
+            }"
+            >favorite</span
+          >
         </button>
       </div>
 
-      <button @click="addCart(book)"
+      <button
+        @click="addCart(book)"
         class="flex justify-center items-center gap-1 bg-[#27AE60] hover:bg-[#219653] transition text-white py-2 px-4 w-full rounded text-sm sm:text-base cursor-pointer"
-        :class="book.saleInfo.listPrice?.amount
-          ? 'bg-[#27AE60] hover:bg-[#219653]'
-          : 'bg-gray-400 hover:bg-gray-500'
-          ">
+        :class="
+          book.saleInfo.listPrice?.amount
+            ? 'bg-[#27AE60] hover:bg-[#219653]'
+            : 'bg-gray-400 hover:bg-gray-500'
+        "
+      >
         <span class="material-symbols-outlined text-base">shopping_cart</span>
         {{ book.saleInfo.listPrice?.amount ? 'Comprar' : 'Resgatar' }}
       </button>
